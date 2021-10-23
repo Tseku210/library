af686ae1e51171673564c6b27f845edb2b0ea2cf
Tseku210 <otgonbaatartsenguun@yahoo.com>
2021-10-23 23:40:55 +0800

    sloppy class version hardcoded

--- script.js
+++ script.js
@@ -1,98 +1,203 @@
-let myLibrary = [];
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-const read = document.querySelector("#read");
-const submitButton = document.querySelector("#submit");
-const mainSection = document.querySelector("#main");
+// let myLibrary = [];
+// const title = document.querySelector("#title");
+// const author = document.querySelector("#author");
+// const pages = document.querySelector("#pages");
+// const read = document.querySelector("#read");
+// const submitButton = document.querySelector("#submit");
+// const mainSection = document.querySelector("#main");
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+// function Book(title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
 
-function addBookToLibrary(){
-    myLibrary.push(new Book(title.value, author.value, pages.value, read.checked))
-    storeLocal()
-}
+// function addBookToLibrary(){
+//     myLibrary.push(new Book(title.value, author.value, pages.value, read.checked))
+//     storeLocal()
+// }
 
-submitButton.addEventListener("click", () => {
-    addBookToLibrary()
-    refreshCards()
-    displayCards()
-    storeLocal()
-})
+// submitButton.addEventListener("click", () => {
+//     addBookToLibrary()
+//     refreshCards()
+//     displayCards()
+//     storeLocal()
+// })
 
-function refreshCards(){
-    while (mainSection.firstChild){
-        mainSection.removeChild(mainSection.firstChild);
+// function refreshCards(){
+//     while (mainSection.firstChild){
+//         mainSection.removeChild(mainSection.firstChild);
+//     }
+//     storeLocal()
+// }
+
+// function displayCards() {
+//     for (let i = 0; i < myLibrary.length; i++){
+//         let div = document.createElement("div");
+//         let titleCard = document.createElement("h2");
+//         let authorCard = document.createElement("p");
+//         let pagesCard = document.createElement("p");
+//         let readCard = document.createElement("input");
+//         let removeButton = document.createElement("button");
+
+//         readCard.type = "checkbox";
+
+//         if (myLibrary[i].read === true){
+//             readCard.checked = true;
+//         } else if (
+//             myLibrary[i].read === false) {
+//             readCard.checked = false;
+//         };
+
+//         authorCard.textContent = myLibrary[i].author;
+//         titleCard.textContent = myLibrary[i].title;
+//         pagesCard.textContent = myLibrary[i].pages;
+//         removeButton.classList.add("remove");
+//         removeButton.textContent = "REMOVE";
+
+//         div.classList.add("card");
+//         div.dataset.index = `${i}`;
+//         removeButton.dataset.index = `${i}`
+
+//         div.append(titleCard);
+//         div.append(authorCard);
+//         div.append(pagesCard);
+//         div.append(readCard);
+//         div.append(removeButton);
+//         mainSection.appendChild(div);
+//         storeLocal()
+//     }
+//     const removeButtons = document.querySelectorAll(".remove");
+//     removeButtons.forEach(button => button.addEventListener("click", (e) => {
+//         myLibrary.splice(Number(e.target.dataset.index), 1);
+//         console.log(Number(e.target.dataset.index))
+//         console.log(myLibrary[Number(e.target.dataset.index)])
+//         console.log(myLibrary)
+//         console.log(e.target)
+//         refreshCards()
+//         displayCards()
+//         storeLocal()
+//     }))
+// }
+// function storeLocal(){
+//     localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+// }
+// function restore(){
+//     if (!localStorage.myLibrary){
+//         displayCards();
+//     } else {
+//         let storageLocal = localStorage.getItem("myLibrary");
+//         let storage = JSON.parse(storageLocal);
+//         myLibrary = storage;
+//         displayCards()
+//     }
+// }
+
+// restore();
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-    storeLocal()
 }
+class MyLibrary {
 
-function displayCards() {
-    for (let i = 0; i < myLibrary.length; i++){
-        let div = document.createElement("div");
-        let titleCard = document.createElement("h2");
-        let authorCard = document.createElement("p");
-        let pagesCard = document.createElement("p");
-        let readCard = document.createElement("input");
-        let removeButton = document.createElement("button");
-
-        readCard.type = "checkbox";
-
-        if (myLibrary[i].read === true){
-            readCard.checked = true;
-        } else if (
-            myLibrary[i].read === false) {
-            readCard.checked = false;
-        };
-
-        authorCard.textContent = myLibrary[i].author;
-        titleCard.textContent = myLibrary[i].title;
-        pagesCard.textContent = myLibrary[i].pages;
-        removeButton.classList.add("remove");
-        removeButton.textContent = "REMOVE";
-
-        div.classList.add("card");
-        div.dataset.index = `${i}`;
-        removeButton.dataset.index = `${i}`
-
-        div.append(titleCard);
-        div.append(authorCard);
-        div.append(pagesCard);
-        div.append(readCard);
-        div.append(removeButton);
-        mainSection.appendChild(div);
-        storeLocal()
+    constructor(){
+        this.myLibrary = [];
+        this.title = document.querySelector("#title");
+        this.author = document.querySelector("#author");
+        this.pages = document.querySelector("#pages");
+        this.read = document.querySelector("#read");
+        this.submitButton = document.querySelector("#submit");
+        this.mainSection = document.querySelector("#main");
     }
-    const removeButtons = document.querySelectorAll(".remove");
-    removeButtons.forEach(button => button.addEventListener("click", (e) => {
-        myLibrary.splice(Number(e.target.dataset.index), 1);
-        console.log(Number(e.target.dataset.index))
-        console.log(myLibrary[Number(e.target.dataset.index)])
-        console.log(myLibrary)
-        console.log(e.target)
-        refreshCards()
-        displayCards()
-        storeLocal()
-    }))
-}
-function storeLocal(){
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-}
-function restore(){
-    if (!localStorage.myLibrary){
-        displayCards();
-    } else {
-        let storageLocal = localStorage.getItem("myLibrary");
-        let storage = JSON.parse(storageLocal);
-        myLibrary = storage;
-        displayCards()
+
+    addBookToLibrary = () => {
+        this.myLibrary.push(new Book(this.title.value, this.author.value, this.pages.value, this.read.checked))
+        this.storeLocal()
+    }
+    submitListener = () => {
+        this.submitButton.addEventListener("click", () => {
+            this.addBookToLibrary()
+            this.refreshCards()
+            this.displayCards()
+            this.storeLocal()
+        })
+    }
+    refreshCards = () => {
+        while (this.mainSection.firstChild){
+            this.mainSection.removeChild(this.mainSection.firstChild);
+        }
+        this.storeLocal()
+    }
+    displayCards = () => {
+        for (let i = 0; i < this.myLibrary.length; i++){
+            let div = document.createElement("div");
+            let titleCard = document.createElement("h2");
+            let authorCard = document.createElement("p");
+            let pagesCard = document.createElement("p");
+            let readCard = document.createElement("input");
+            let removeButton = document.createElement("button");
+    
+            readCard.type = "checkbox";
+    
+            if (this.myLibrary[i].read === true){
+                readCard.checked = true;
+            } else if (
+                this.myLibrary[i].read === false) {
+                readCard.checked = false;
+            };
+    
+            authorCard.textContent = this.myLibrary[i].author;
+            titleCard.textContent = this.myLibrary[i].title;
+            pagesCard.textContent = this.myLibrary[i].pages;
+            removeButton.classList.add("remove");
+            removeButton.textContent = "REMOVE";
+    
+            div.classList.add("card");
+            div.dataset.index = `${i}`;
+            removeButton.dataset.index = `${i}`
+    
+            div.append(titleCard);
+            div.append(authorCard);
+            div.append(pagesCard);
+            div.append(readCard);
+            div.append(removeButton);
+            this.mainSection.appendChild(div);
+            this.storeLocal()
+        }
+        const removeButtons = document.querySelectorAll(".remove");
+        removeButtons.forEach(button => button.addEventListener("click", (e) => {
+            this.myLibrary.splice(Number(e.target.dataset.index), 1);
+            console.log(Number(e.target.dataset.index))
+            console.log(this.myLibrary[Number(e.target.dataset.index)])
+            console.log(this.myLibrary)
+            console.log(e.target)
+            this.refreshCards()
+            this.displayCards()
+            this.storeLocal()
+        }))
+    }
+    storeLocal = () => {
+        localStorage.setItem("myLibrary", JSON.stringify(this.myLibrary));
+    }
+    restore = () => {
+        if (!localStorage.myLibrary){
+            this.displayCards();
+        } else {
+            let storageLocal = localStorage.getItem("myLibrary");
+            let storage = JSON.parse(storageLocal);
+            this.myLibrary = storage;
+            this.displayCards()
+        }
+    }
+    run = () => {
+        this.submitListener()
+        this.restore()
     }
 }
-
-restore();
-
+let library = new MyLibrary;
+library.run()
